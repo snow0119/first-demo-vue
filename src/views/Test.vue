b0f674e06f3b2366ff67b9428226efd7477ca271
lixiaoxue <lixx@raiyee.com>
2017-11-12 21:39:22 +0800

    test axios

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,7 +2,25 @@
   div(class = 'ha')
     h1 我是标题
     span 来测试pug模板了~~~
+    button sendPost
 </template>
+<script>
+  export default {
+    name: 'test',
+    methods: {
+      postData () {
+        this.$http({
+          method: 'post',
+          url: '/user',
+          data: {
+            name: 'snow',
+            info: '12'
+          }
+        })
+      }
+    }
+  }
+</script>
 <style>
   .ha {
     color: white;
