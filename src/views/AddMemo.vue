1a2c7d3db4ea0bb61e327d354fa8702b8da82170
lixiaoxue <lixx@raiyee.com>
2018-01-14 12:07:24 +0800

    modify addMemo style

--- src/views/AddMemo.vue
+++ src/views/AddMemo.vue
@@ -4,18 +4,19 @@
     ul
       li
         span 模块名：
-        input(v-model="memoName", placeholder="请输入模块名")
+        input(class="memoName", v-model="memoName", placeholder="请输入模块名")
       li
         span 需求描述：
-        input(v-model="demand", placeholder="请输入需求的相关描述")
+        input(class="demand", v-model="demand", placeholder="请输入需求的相关描述")
       li
         span 需完成项:
         div(class="remark")
           textarea(v-model="currentItem", placeholder="请输入需要完成的项")
         button(class="add-item", @click="addItem") 添加
-        template(v-for="(item, index) in items")
-          input(type="checkbox", @click="itemFinished(item.detail)", :checked="item.selected")
-          span {{item.detail}}
+        div(class="items", v-if="items.length > 0")
+          div(class="item", v-for="(item, index) in items")
+            input(type="checkbox", @click="itemFinished(item.detail)", :checked="item.selected")
+            div {{item.detail}}
       button(class="save-memo", @click="saveMemo") 保存
 </template>
 <script>
@@ -59,7 +60,7 @@
     background-color: rgba(255, 255, 255, 0.6);
   }
 
-  .container-add li input {
+  .container-add li .memoName, .container-add li .demand {
     position: relative;
     width: 50%;
     margin-top: 10px;
@@ -111,11 +112,55 @@
     border: 1px solid #f1f1f1;
   }
 
+  .container-add .items {
+    margin-left: 80px;
+  }
+
+  .container-add .items input {
+    width: 5px;
+  }
+
   .container-add .add-item {
     position: absolute;
     top: 20px;
     width: 50px;
     margin-left: 10px;
+  }
+
+  .container-add .items, .container-add .item div {
+    display: inline-block;
+  }
+
+  .container-add .items {
+    margin-top: 10px;
+  }
+
+  .container-add .item:nth-of-type(odd) {
+    background-color: #c3e6e8;
+  }
+
+  .container-add .item:nth-of-type(even) {
+    background-color: #d8e8cb;
+  }
+
+  .container-add .item {
+    position: relative;
+    padding: 0 6px;
+    background-color: white;
+  }
+
+  .container-add .item input {
+    position: absolute;
+    top: 50%;
+    transform: translate3d(0, -50%, 0);
+  }
+
+  .container-add .item div {
+    width: 420px;
+    padding: 5px;
+    font-size: 12px;
+    text-align: left;
+    margin-left: 20px;
   }
 
   .container-add .save-memo {
